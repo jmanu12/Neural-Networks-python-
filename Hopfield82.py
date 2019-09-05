6be59071a43fc2168acb5eebd72ca67946700998
jmanu12 <juan.fajardo@fielo.com>
2019-09-05 16:46:26 -0300

    for severelat paterns

--- Hopfield82.py
+++ Hopfield82.py
@@ -1,23 +1,31 @@
 import matplotlib as matplotlib
 import numpy as np
 from tqdm import tqdm
+
+
 class Hopfield82:
-    def create_weight_matrix(self,x):
+    def create_weight_matrix(self, M):
         """ Create a weigth matrix
-            input x: is a vector of the patern
-            output w: weight matrix
+            input M: is a paterns matrix where each row is an input
+            output w: weight matrix trained
         """
-        if len(x.shape) != 1:
-            print("The input is not vector")
+        if M.shape[0] < 1 or M.shape[0] == None:
+            print("Error")
             return
         else:
-            w = np.zeros([len(x), len(x)])
-            for i in range(len(x)):
-                for j in range(i, len(x)):
-                    if i == j:
-                        w[i, j] = 0
-                    else:
-                        w[i, j] = x[i] * x[j]
-                        # by symmetry
-                        w[j, i] = w[i, j]
-        return w
+            c = 0
+            for p in range(M.shape[0]):
+                x = M.A[p]
+                w = np.zeros([len(x), len(x)])
+                for i in range(len(x)):
+                    for j in range(i, len(x)):
+                        if i == j:
+                            w[i, j] = 0
+                        else:
+                            w[i, j] = x[i] * x[j]
+                            # by symmetry
+                            w[j, i] = w[i, j]
+                c = c + w
+                print(c)
+        # is divided for the number of paterns
+        return c * 1 / M.shape[0]
--- hopfieldPatternRecognition.py
+++ hopfieldPatternRecognition.py
@@ -3,11 +3,16 @@ import numpy as np
 from tqdm import tqdm
 from Hopfield82 import Hopfield82
 #The imput vectors are the paterns P1, P2
-E1 = np.array([1,1,-1,-1])
-E2 = np.array([-1, -1, 1, 1])
+M = np.matrix("1,1,-1,-1; -1,-1,1,1")
 ##############LEARNING######################
 w = Hopfield82()
-r = w.create_weight_matrix(E1)
-print(r)
+R = w.create_weight_matrix(M)
+print('************************')
+print(R)
+
+
+
+
+
 
     #https://github.com/yosukekatada/Hopfield_network/blob/master/hopfield.py
